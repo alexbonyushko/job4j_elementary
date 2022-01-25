3c408658e3d4fc8a30ee1e11326139bd4d68debc
a.bonyushko <a.bonyushko@cdek.ru>
2022-01-25 19:10:02 +0300

    5.5. Простое число [#171694]

---
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,20 @@
+package ru.job4j.loop;
+
+/**
+ * 5.5. Простое число [#171694]
+ **/
+public class CheckPrimeNumber {
+    public static boolean check(int number) {
+        boolean prime = false;
+        for (int i = 2; i <= number - 1; i++) {
+            if (number % i == 0) {
+                prime = false;
+                break;
+            } else if ((number % number == 0) && (number % 1 == 0)) {
+                prime = true;
+                break;
+            }
+        }
+        return prime;
+    }
+}
