8aad53cd3962c5bf170253b0bd603965174a9c00
a.bonyushko <a.bonyushko@cdek.ru>
2022-01-26 23:19:25 +0300

    5.6. Простые числа [#156314]

---
+++ src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -0,0 +1,16 @@
+package ru.job4j.loop;
+
+/**
+ * 5.6. Простые числа [#156314]
+ */
+public class PrimeNumber {
+    public static int calc(int finish) {
+        int count = 0;
+        for (int number = 1; number <= finish; number++) {
+            if (CheckPrimeNumber.check(number)) {
+                count = count + 1;
+            }
+        }
+        return count;
+    }
+}
---
+++ src/test/java/ru/job4j/loop/PrimeNumberTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.loop;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+/**
+ * 5.6. Тесты - Простые числа [#156314]
+ */
+public class PrimeNumberTest {
+
+    @Test
+    public void when5() {
+        int finish = 5;
+        int result = PrimeNumber.calc(finish);
+        int expected = 3;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void when11() {
+        int finish = 11;
+        int result = PrimeNumber.calc(finish);
+        int expected = 5;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void when2() {
+        int finish = 2;
+        int result = PrimeNumber.calc(finish);
+        int expected = 1;
+        Assert.assertEquals(expected, result);
+    }
+}
