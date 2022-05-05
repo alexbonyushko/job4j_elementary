e47466916aebc79a466a0ac3aafe88527b153198
alexbonyushko <a.bonyushko@gmail.com>
2022-05-05 11:36:20 +0300

    5.7. Ипотека [#156315] - исправления

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -6,9 +6,8 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, int salary, double persent) {
         int year = 0;
-        while (amount + amount * (persent / 100) >= 0) {
-            double duty = amount + amount * (persent / 100);
-            amount = duty - salary;
+        while (amount > 0) {
+            amount = amount + amount * (persent / 100) - salary;
             year++;
         }
         return year;
