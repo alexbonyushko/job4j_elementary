514ea1356fc0cba4c8dca177b0b1403d82319195
alexbonyushko <a.bonyushko@gmail.com>
2022-05-19 18:19:52 +0300

    5.9.1. Генерация конструкций в IDEA [#279134]

---
+++ src/main/java/ru/job4j/live/Loop.java
@@ -0,0 +1,12 @@
+package ru.job4j.live;
+/**
+ * 5.9.1. Генерация конструкций в IDEA [#279134]
+ */
+
+public class Loop {
+    public static void main(String[] args) {
+        for (int i = 0; i <=10 ; i++) {
+            System.out.println(i);
+        }
+    }
+}
