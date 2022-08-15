bc49f49e57c97fd3d3bae4311556a332ee7b1f2b
alexbonyushko <a.bonyushko@gmail.com>
2022-08-15 18:23:47 +0200

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -3,6 +3,7 @@ package ru.job4j.array;
 /**
  * 6.0.1. Объявление массива.[#3613]
  * 6.0.2. Размер массива [#333582]
+ * 6.0.3. Заполнение массива. [#226300]
  */
 public class ArrayDefinition {
     public static void main(String[] args) {
@@ -12,5 +13,14 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Размер массива равен: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Alex";
+        names[1] = "Jonathan";
+        names[2] = "Iakov";
+        names[3] = "David";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
