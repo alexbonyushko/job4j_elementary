3cfaaca6b2a857fd778a47cf4fbc28907a29f237
alexbonyushko <a.bonyushko@gmail.com>
2022-08-16 17:41:57 +0200

    6.0.5. Массивы и цикл for-each [#333581]

--- src/main/java/ru/job4j/array/ArrayLoop.java
+++ src/main/java/ru/job4j/array/ArrayLoop.java
@@ -2,6 +2,7 @@ package ru.job4j.array;
 
 /**
  * 6.0.4. Массивы и цикл for. [#226665]
+ * 6.0.5. Массивы и цикл for-each [#333581]
  */
 public class ArrayLoop {
 
@@ -10,8 +11,8 @@ public class ArrayLoop {
         for (int index = 0; index < numbers.length; index++) {
             numbers[index] = index * 2 + 3;
         }
-        for (int index = 0; index < numbers.length; index++) {
-            System.out.println(numbers[index]);
+        for (int num : numbers) {
+            System.out.println(num);
         }
     }
 }
